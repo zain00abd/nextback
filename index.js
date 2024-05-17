c48e52a7e012c335b38d28e529c4a4c800494eac
zain00abd <zainabflash1998@gmail.com>
2024-05-17 14:37:40 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+
+const index = express()
+
+const mongoose = require("mongoose")
+
+const port = 3001
+const AdduserModal = require("./module")
+
+index.get('/', (req, res) => {
+
+  AdduserModal.find()
+      .then((result) => {
+        res.json(result);
+
+
+      })
+      .catch((error) => {
+
+      });
+});
+
+mongoose.connect("mongodb+srv://zaindiv:Zain007abd@cluster0.32r5dqe.mongodb.net/all-data?retryWrites=true&w=majority")
+    .then(() => {
+      index.listen(port, () => {
+            console.log(`http://localhost:${port}/`)
+        })
+    })
+    .catch(() => {
+
+
+    })
---
+++ module.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+ const Schema = mongoose.Schema;
+ const models = mongoose.models;
+
+ // define the Schema (the structure of the article)
+ const productSchema = new Schema({
+   name: String,
+   addres: String,
+   phone: String,
+
+ });
+
+ // Create a model based on that schema
+ const AdduserModal = models.Product || mongoose.model("Product", productSchema);
+
+ // export the model
+ module.exports = AdduserModal;
