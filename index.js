87d68af9d6f8beb826145e6b80162276b46750f8
zain00abd <zainabflash1998@gmail.com>
2024-05-17 14:46:20 +0300

    edit server

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const index = express()
+const index = express();
+index.use(cors());
 
-index.use(cors)
-
-const mongoose = require("mongoose")
-
-const port = 3001 || process.env.PORT
-const AdduserModal = require("./module")
+const port = process.env.PORT || 3001;
+const AdduserModal = require("./module");
 
 index.get('/', (req, res) => {
-
   AdduserModal.find()
-      .then((result) => {
-        res.json(result);
-
-
-      })
-      .catch((error) => {
-
-      });
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((error) => {
+      res.status(500).send(error.message); // Respond with error message
+    });
 });
 
-mongoose.connect("mongodb+srv://zaindiv:Zain007abd@cluster0.32r5dqe.mongodb.net/all-data?retryWrites=true&w=majority")
-    .then(() => {
-      index.listen(port, () => {
-            console.log(`http://localhost:${port}/`)
-        })
-    })
-    .catch(() => {
-
-
-    })
+mongoose.connect("mongodb+srv://zaindiv:Zain007abd@cluster0.32r5dqe.mongodb.net/all-data?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    index.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((error) => {
+    console.error('Database connection error:', error); // Log error to the console
+  });
