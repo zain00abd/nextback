4d62831b81439a089c86b225a535c4a5938219c7
zain00abd <zainabflash1998@gmail.com>
2024-08-09 19:18:21 +0300

    ss

--- models/ArrCustomerModal.js
+++ models/ArrCustomerModal.js
@@ -3,15 +3,7 @@ const Schema = mongoose.Schema;
 const models = mongoose.models;
 
 const invoiceSchema = new Schema({
-  customers: [
-    {
-      name:String,
-      phone:String,
-      addres:String,
-      arrinvoice:Array,
-    }
-
-  ],
+  customers: [],
 });
 
 const invoiceModal = models.invoicecustomer || mongoose.model('invoicecustomer', invoiceSchema);
